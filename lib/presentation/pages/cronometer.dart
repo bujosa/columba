4ec3feaf8fe8ea254e0893d9ed0e151f63b4d028
bujosa <davidbujosa@gmail.com>
2023-01-01 02:53:21 -0400

    feat: finish time provider logic

--- lib/presentation/pages/cronometer.dart
+++ lib/presentation/pages/cronometer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/timer_provider.dart';
 
 class Cronometer extends StatefulWidget {
   const Cronometer({Key? key}) : super(key: key);
@@ -10,13 +12,46 @@ class Cronometer extends StatefulWidget {
 class _CronometerState extends State<Cronometer> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.black,
-        alignment: Alignment.center,
-        child: const Text('Hello World',
-            style: TextStyle(color: Colors.white, fontSize: 30)),
-      ),
+    final timerProvider = Provider.of<TimerProvider>(context);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          context
+              .select((TimerProvider timeProvider) => timeProvider.currentTime),
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 60,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                timerProvider.startTimer();
+              },
+              child: const Text('Start'),
+            ),
+            const SizedBox(width: 20),
+            ElevatedButton(
+              onPressed: () {
+                timerProvider.pauseTimer();
+              },
+              child: const Text('Pause'),
+            ),
+            const SizedBox(width: 20),
+            ElevatedButton(
+              onPressed: () {
+                timerProvider.restartTimer();
+              },
+              child: const Text('Restart'),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
--- lib/presentation/providers/timer_provider.dart
+++ lib/presentation/providers/timer_provider.dart
@@ -1,12 +1,19 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class TimerProvider extends ChangeNotifier {
-  Duration duration = const Duration(seconds: 60);
+  late Timer timer;
+  int time = 0;
   bool isPaused = false;
+  bool isRunning = false;
 
   void startTimer() {
     isPaused = false;
-    notifyListeners();
+    isRunning = true;
+    timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
+      isPaused ? timer.cancel() : time += 100;
+      notifyListeners();
+    });
   }
 
   void pauseTimer() {
@@ -17,13 +24,34 @@ class TimerProvider extends ChangeNotifier {
   }
 
   void restartTimer() {
-    duration = const Duration(seconds: 60);
+    time = 0;
+    isPaused = false;
+    isRunning = false;
     notifyListeners();
   }
 
   void resumeTimer() {
     if (!isPaused) return;
     isPaused = false;
+    startTimer();
     notifyListeners();
+  }
+
+  String get currentTime {
+    if (!isRunning) {
+      return '00:00:00:00';
+    }
+    // Miliseconds to hours, minutes, seconds and miliseconds
+    final hours = ((time / 1000) / 60 / 60).floor().toString().padLeft(2, '0');
+    final minutes = ((time / 1000) / 60).floor().toString().padLeft(2, '0');
+    final seconds = ((time / 1000) % 60).floor().toString().padLeft(2, '0');
+    final milliseconds = (time % 1000).floor().toString().padLeft(2, '0');
+
+    // final hours = ((time / 60) / 60).floor().toString().padLeft(2, '0');
+    // final minutes = ((time / 60) % 60).floor().toString().padLeft(2, '0');
+    // final seconds = (time % 60).floor().toString().padLeft(2, '0');
+    // final milliseconds = (time % 100).floor().toString().padLeft(2, '0');
+
+    return "$hours:$minutes:$seconds:$milliseconds";
   }
 }
